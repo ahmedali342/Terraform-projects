3fbfbad143493a34030fd20749ff93592b06b8a2
ahmedali342 <ahmedhali432@gmail.com>
2025-05-10 13:28:19 +0530

    yaml

--- ec2/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "public_ip" {
-  value = aws_instance.dev_instance.public_ip
-}
-
-output "ssh_command" {
-  value = "ssh -i ssh-key/key ec2-user@${aws_instance.dev_instance.public_ip}"
-}
--- terragrunt/dev/s3/terragrunt.hcl
+++
@@ -1,7 +0,0 @@
-terraform {
-  source = "C:\\Ahmed\\Devops\\Devops\\Terraform\\Circle Ci\\s3-bucket"
-}
-
-inputs = {
-  bucket_name = "my-circleci-artifacts-dev"
-}
